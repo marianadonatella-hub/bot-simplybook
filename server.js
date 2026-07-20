df673744d7ae9b098ad79c250ccb8225eb02cf7a
marianadonatella-hub <marianadonatella@gmail.com>
2026-07-20 21:29:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ app.post('/rezerva', async (req, res) => {
         const oraCuSecunde = `${time}:00`; 
 
         // FORȚĂM ID-UL 1 PENTRU MEDIC ȘI SERVICIU (Să apară sigur în calendarul tău principal!)
-        const sId = 1;
+        const sId = 0;
         const pId = 1;
 
         const response = await axios.post(apiUrl, {
